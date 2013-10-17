d7bb07f4ea9eb71e796b625064c40e9542ba4ca5
mawujun1234 <mawujun1234@163.com>
2013-10-17 15:48:19 +0800

    fff

---
+++ leon-web-common/src/main/webapp/desktop/monitor/CpuInfoPanel.js
@@ -0,0 +1,14 @@
+Ext.define('Leon.desktop.monitor.CpuInfoPanel',{
+	extend:'Ext.panel.Panel',
+	title:'概要',
+	border:false, 
+	//frame:true,
+	initComponent:function(){
+		var me=this;
+		var chart=Ext.create
+		var form=Ext.create('Ext.form.Panel',{
+		
+		});
+		me.callParent();
+	}
+});
