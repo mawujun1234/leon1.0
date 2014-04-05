a6c9753c57c574b051199e6533b8d593a69f4f36
mawujun1234 <mawujun1234@163.com>
2014-04-05 10:42:20 +0800

    111

---
+++ leon-infrastructure/src/main/java/com/mawujun/service/AbstractService.java
@@ -0,0 +1,127 @@
+package com.mawujun.service;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import com.mawujun.repository.BaseRepository;
+import com.mawujun.repository.cnd.Cnd;
+import com.mawujun.repository.idEntity.IdEntity;
+import com.mawujun.repository1.IRepository;
+import com.mawujun.utils.page.PageRequest;
+import com.mawujun.utils.page.QueryResult;
+
+public abstract class AbstractService<T extends IdEntity<ID>, ID extends Serializable> {
+	/**
+	 * 用来返回默认的Repository
+	 * @author mawujun 16064988@qq.com 
+	 * @return
+	 */
+	public abstract IRepository<T,ID> getRepositoryMapper();
+	
+	public ID create(T entity) {
+		return this.getRepositoryMapper().create(entity);
+	}
+	
+	/**
+	 * 通过Cnd。update().set(...)。andEquals();来指定更新的字段和条件
+	 * @author mawujun email:16064988@163.com qq:16064988
+	 * @param cnd
+	 */
+	public void update(Cnd cnd) {
+		this.getRepositoryMapper().update(cnd);
+	}
+	/**
+	 * 动态更新，对有值的字段进行更新，即如果字段=null，那就不进行更新
+	 * @param entity
+	 */
+	public void updateIgnoreNull(final T entity) {
+		this.getRepositoryMapper().updateIgnoreNull(entity);
+	}
+	
+	
+	public void delete(T entity) {
+		this.getRepositoryMapper().delete(entity);
+	}
+	public void deleteById(ID id) {
+		this.getRepositoryMapper().deleteById(id);
+	}
+	public T get(ID id) {
+		return this.getRepositoryMapper().get(id);
+	}
+	
+	public Serializable[] createBatch(final T... entities) {
+		return this.getRepositoryMapper().createBatch(entities);
+	}
+	public Serializable[] createBatch(final Collection<T> entities) {
+		return this.getRepositoryMapper().createBatch(entities);
+	}
+	
+	/**
+	 * 注意，如果某个属性没有设置值，将会更新为null
+	 * @author mawujun 16064988@qq.com 
+	 * @param entities
+	 * @return
+	 */
+	public int updateBatch(final T... entities) {
+		return this.getRepositoryMapper().updateBatch(entities);
+	}
+	public int updateBatch(final Collection<T> entities) {
+		return this.getRepositoryMapper().updateBatch(entities);
+	}
+	
+	public int deleteAll() {
+		return this.getRepositoryMapper().deleteAll();
+	}
+	/**
+	 * 注意，使用Cnd的地方表示删除的是泛型指定的类。
+	 * @author mawujun email:16064988@163.com qq:16064988
+	 * @param cnd
+	 * @return
+	 */
+	public int deleteBatch(Cnd cnd) {
+		return this.getRepositoryMapper().deleteBatch(cnd);
+	}
+	public int deleteBatch(final Collection<T> entities) {
+		return this.getRepositoryMapper().deleteBatch(entities);
+	}
+	public int deleteBatch(final T... entities) {
+		return this.getRepositoryMapper().deleteBatch(entities);
+	}
+	public int deleteBatch(final ID... IDS) {
+		return this.deleteBatch(IDS);
+	}
+	
+	
+	public Long queryCount(Cnd cnd) {
+		return this.queryCount(cnd);
+	}
+	public Object queryMax(String property,Cnd cnd) {
+		return this.queryMax(property, cnd);
+	}
+	public Object queryMin(String property,Cnd cnd) {
+		return this.queryMin(property, cnd);
+	}
+	/**
+	 * 返回第一个对象
+	 * @author mawujun email:mawujun1234@163.com qq:16064988
+	 * @param whereInfos
+	 * @return
+	 */
+	public T queryUnique(Cnd cnd) {
+		return this.queryUnique(cnd);
+	}
+	public <M> M queryUnique(Cnd cnd,Class<M> classM) {
+		return this.queryUnique(cnd, classM);
+	}
+	
+	public List<T> queryAll() {
+		return this.queryAll();
+	}
+	public List<T> query(Cnd cnd) {
+		return this.query(cnd);
+	}
+	public QueryResult<T> queryPage(final PageRequest pageRequest) {
+		return this.queryPage(pageRequest);
+	}
+}
