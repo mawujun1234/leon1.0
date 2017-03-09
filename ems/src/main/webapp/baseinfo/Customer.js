d062ef6a8b03310e7fcf0d3cfe146a8c6a6af401
mawujun1234 <mawujun1234@163.com>
2017-03-09 13:50:59 +0800

    121

--- ems/src/main/webapp/baseinfo/Customer.js
+++ ems/src/main/webapp/baseinfo/Customer.js
@@ -10,6 +10,27 @@ Ext.define("Ems.baseinfo.Customer",{
 		{name:'status',type:'bool'},
 		{name:'type',type:'int'}
 	],
-	associations:[
-	]
+	proxy:{
+		type:'ajax',
+		actionMethods: { read: 'POST' },
+		timeout :600000,
+		headers:{ 'Accept':'application/json;'},
+		writer:{
+			type:'json',
+			writeRecordId:true,
+			writeAllFields:true
+		},
+		reader:{
+			type:'json'
+//			,rootProperty:'root',
+//			successProperty:'success',
+//			totalProperty:'total'		
+		},
+		api:{
+			read:Ext.ContextPath+'/customer/load.do',
+			create:Ext.ContextPath+'/customer/create.do',
+			update:Ext.ContextPath+'/customer/update.do',
+			destroy:Ext.ContextPath+'/customer/destroy.do'
+		}
+	}
 });
